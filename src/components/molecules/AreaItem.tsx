d92b164b05d7dd37524a8d184aad1a64f2fde33c
Andrea <andreagoal96@gmail.com>
2025-07-15 11:00:59 +0200

    created first pages and components based on figma file

---
+++ src/components/molecules/AreaItem.tsx
@@ -0,0 +1,7 @@
+export default function AreaItem() {
+    return (
+    <>
+        
+    </>
+    )
+}
---
+++ src/components/molecules/NumberOfTasks.tsx
@@ -0,0 +1,7 @@
+export default function NumberOfTasks() {
+    return (
+    <>
+        
+    </>
+    )
+}
