ad3015f0a02ecd2621e4bc2d22f98f52181dc6dd
Andrea Gomez Alvarez <andreagoal96@gmail.com>
2025-07-17 16:09:15 +0200

    created logo

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,7 +1,20 @@
-export default function Logo() {
+import Title from '../typography/Title';
+
+interface Props {
+    className?: string;
+    size?: 'large' | '2xlarge';
+}
+
+export default function Logo(props: Props) {
+    const { size = 'large', className } = props;
+    const variant =
+        size === '2xlarge' ? 'title-2xlarge-light' : 'title-large-light';
+
     return (
-    <>
-        
-    </>
-    )
+        <>
+            <Title as="h1" variant={variant} className={className}>
+                NeatNest
+            </Title>
+        </>
+    );
 }
