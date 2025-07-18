63cac8774c4cfefa435a1a8173ec2559bbc1232f
Andrea Gomez Alvarez <andreagoal96@gmail.com>
2025-07-18 17:50:58 +0200

    modified Button component

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -7,16 +7,33 @@ interface Props {
     type?: 'submit' | 'button' | 'reset';
     children?: ReactNode;
     buttonVariant?: 'primary' | 'secondary';
-    textVariant: 'body-large-regular' | 'body-large-bold';
+    textVariant?: 'body-large-regular' | 'body-large-bold';
     icon?: ReactNode;
 }
 
 const buttonVariants = {
-    primary:
-        'inline-flex px-4 py-2 justify-center items-center gap-[0.5rem] rounded-xl border border-white/20 bg-gradient-to-b from-[#3FF69E] to-[#97FFCC] shadow-[0px_2px_4px_rgba(62,52,69,0.16)]',
-
-    secondary:
-        '"inline-flex px-4 py-2 justify-center items-center gap-[0.5rem] rounded-xl border-2 border-[#2AD482] bg-white bg-[linear-gradient(180deg,rgba(255,255,255,0.3)_7.29%,rgba(255,255,255,0)_65.62%),#ffffff]"',
+    primary: `
+    inline-flex justify-center items-center gap-[0.5rem]
+    px-4 py-2
+    rounded-xl
+    shadow-xs
+    bg-brand-primary
+    hover:bg-brand-secondary hover:shadow-sm
+    focus:bg-brand-secondary focus:shadow-none
+    focus:outline-2 focus:outline-brand-primary
+    focus-visible:outline-2 focus-visible:outline-brand-primary
+  `,
+    secondary: `
+    inline-flex justify-center items-center gap-[0.5rem]
+    px-4 py-2
+    rounded-xl
+    shadow-xs
+    bg-neutral-secondary
+    outline-2 outline-brand-primary
+    hover:bg-neutral-tertiary hover:shadow-sm
+    focus:bg-neutral-tertiary focus:outline-3
+    focus-visible:outline-3
+  `,
 };
 
 export default function Button(props: Props) {
@@ -29,16 +46,21 @@ export default function Button(props: Props) {
         children,
         ...rest
     } = props;
+
+    const variantDefault = {
+        primary: 'body-large-bold',
+        secondary: 'body-large-regular',
+    } as const;
+
+    const finalTextVariant = textVariant || variantDefault[buttonVariant];
     const classes = twMerge(buttonVariants[buttonVariant], className);
 
     return (
-        <>
-            <button type={type || 'button'} className={classes} {...rest}>
-                {icon && <span>{icon}</span>}
-                <BodyText as="span" variant={textVariant}>
-                    {children}
-                </BodyText>
-            </button>
-        </>
+        <button type={type} className={classes} {...rest}>
+            {icon && <span>{icon}</span>}
+            <BodyText as="span" variant={finalTextVariant}>
+                {children}
+            </BodyText>
+        </button>
     );
 }
