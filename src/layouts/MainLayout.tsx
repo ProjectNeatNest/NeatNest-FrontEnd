ddbdaea70e7f313859ec5ab94b7410170ac48109
Andrea <andreagoal96@gmail.com>
2025-07-21 12:54:18 +0200

    overflow-hidden in MainLayout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,24 +1,7 @@
-import { PiPlus } from 'react-icons/pi';
-import Button from '../components/atoms/Button';
-import BodyText from '../components/typography/BodyText';
-import Title from '../components/typography/Title';
-
 export default function MainLayout() {
     return (
         <>
-            <Title as="h1" variant="title-xlarge-semibold">
-                Proyecto Final
-            </Title>
-            <BodyText as="p" variant="body-medium-regular">
-                Este es un párrafo
-            </BodyText>
-            <Button
-                buttonVariant="primary"
-                textVariant="body-large-regular"
-                icon={<PiPlus size={24} fontWeight={'light'} />}
-            >
-                label
-            </Button>
+            <div className="overflow-hidden"></div>
         </>
     );
 }
