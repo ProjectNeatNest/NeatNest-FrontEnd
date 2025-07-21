635044b5adf037c7d1b3c3e7e49e3a4b527c5740
Andrea <andreagoal96@gmail.com>
2025-07-21 13:52:26 +0200

    created Header

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -1,7 +1,19 @@
+import { PiListLight } from 'react-icons/pi';
+import Button from '../atoms/Button';
+import Logo from '../atoms/Logo';
+
 export default function Header() {
     return (
-    <>
-        
-    </>
-    )
+        <>
+            <header className="flex justify-between px-6 py-3">
+                <Logo />
+                <Button
+                    buttonVariant="tertiary"
+                    className="px-2 py-2 shadow-none aspect-square"
+                >
+                    <PiListLight size={32} />
+                </Button>
+            </header>
+        </>
+    );
 }
