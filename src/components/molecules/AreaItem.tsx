1402c4e5c9fe749ad35d95516f086da658e9c059
Andrea <andreagoal96@gmail.com>
2025-07-21 10:48:13 +0200

    created areaItem component

--- src/components/molecules/AreaItem.tsx
+++ src/components/molecules/AreaItem.tsx
@@ -1,7 +1,28 @@
-export default function AreaItem() {
+import Title from '../typography/Title';
+import NumberOfTasks from './NumberOfTasks';
+
+interface Props {
+    areaName: string;
+    completedTasks: number;
+    totalTasks: number;
+}
+
+export default function AreaItem(props: Props) {
+    const { areaName, completedTasks, totalTasks } = props;
+
     return (
-    <>
-        
-    </>
-    )
+        <>
+            <div className="flex flex-col items-center justify-center px-4 py-2 bg-neutral-tertiary rounded-xl min-w-[108px] max-w-[130px] aspect-square shadow-mds">
+                <Title as="h3" variant="title-xsmall-regular">
+                    {areaName}
+                </Title>
+                <div className="flex items-end gap-1 justify-baseline text-neutral-secondary">
+                    <NumberOfTasks
+                        completed={completedTasks}
+                        total={totalTasks}
+                    ></NumberOfTasks>
+                </div>
+            </div>
+        </>
+    );
 }
