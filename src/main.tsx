863152e6906371ffabb6b8f984445c0838371ecf
Andrea <andreagoal96@gmail.com>
2025-07-22 10:40:01 +0200

    applied UserProvider to main.ts

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import MainLayout from './layouts/MainLayout.tsx';
+import UserProvider from './contexts/UserProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <MainLayout />
+        <UserProvider>
+            <MainLayout />
+        </UserProvider>
     </StrictMode>
 );
