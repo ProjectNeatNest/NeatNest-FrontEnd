b84309ad3f04423d43dfa0331df44059b9209889
Andrea Gomez Alvarez <andreagoal96@gmail.com>
2025-07-22 16:01:58 +0200

    created footer

--- src/components/molecules/Footer.tsx
+++ src/components/molecules/Footer.tsx
@@ -1,7 +1,22 @@
+import BodyText from '../typography/BodyText';
+
 export default function Footer() {
     return (
-    <>
-        
-    </>
-    )
+        <footer className="flex flex-col justify-center align-center gap-0.5 py-4 md:py-8">
+            <BodyText
+                as="span"
+                variant="body-small-regular"
+                className="text-neutral-secondary"
+            >
+                Created by NARL with 💚
+            </BodyText>
+            <BodyText
+                as="span"
+                variant="body-small-regular"
+                className="text-neutral-secondary"
+            >
+                2025
+            </BodyText>
+        </footer>
+    );
 }
