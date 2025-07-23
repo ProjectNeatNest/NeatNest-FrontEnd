e1d3593b7a515b0463cf45e8a81061cb421feaa6
Andrea <andreagoal96@gmail.com>
2025-07-23 11:53:47 +0200

    created LoginForm and LoginPage

--- src/components/organisms/LoginForm.tsx
+++ src/components/organisms/LoginForm.tsx
@@ -1,7 +1,43 @@
+import { PiEnvelopeLight, PiLockLight } from 'react-icons/pi';
+import InputField from '../atoms/InputField';
+import Button from '../atoms/Button';
+import BodyText from '../typography/BodyText';
+
 export default function LoginForm() {
     return (
-    <>
-        
-    </>
-    )
+        <form className="flex flex-col items-center justify-center w-full gap-4">
+            <div className="flex flex-col gap-2">
+                <InputField
+                    type="email"
+                    leftIcon={<PiEnvelopeLight size={24} />}
+                    label="Email"
+                    placeholder="Tu email"
+                />
+                <InputField
+                    type="password"
+                    leftIcon={<PiLockLight size={24} />}
+                    label="Contraseña"
+                    placeholder="Tu contraseña"
+                />
+            </div>
+            <Button buttonVariant="primary">Iniciar sesión</Button>
+            <div>
+                <BodyText
+                    as="span"
+                    variant="body-medium-regular"
+                    className="text-neutral-primary"
+                >
+                    ¿No tienes cuenta?{' '}
+                </BodyText>
+                <BodyText
+                    as="a"
+                    variant="body-medium-bold"
+                    className="underline text-neutral-primary"
+                    href="/register"
+                >
+                    Regístrate
+                </BodyText>
+            </div>
+        </form>
+    );
 }
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,18 @@
+import LoginForm from '../components/organisms/LoginForm';
+import BodyText from '../components/typography/BodyText';
+import rafiki from '/rafiki.svg';
+
 export default function LoginPage() {
     return (
-    <>
-        
-    </>
-    )
+        <div className="flex flex-col gap-6">
+            <BodyText
+                as="p"
+                variant="body-large-regular"
+                className="text-center text-neutral-primary"
+            >
+                Entra en tu cuenta para organizar tu hogar
+            </BodyText>
+            <LoginForm />
+        </div>
+    );
 }
