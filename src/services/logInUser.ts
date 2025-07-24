cef274d20064aba0e982b61205225de56f70665f
Andrea <andreagoal96@gmail.com>
2025-07-24 12:33:15 +0200

    created function loginUser

---
+++ src/services/logInUser.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosError } from 'axios';
+import { toast } from 'react-toastify';
+import type { LogInResponse } from '../config/types';
+
+export async function logInUser(
+    email: string,
+    password: string
+): Promise<LogInResponse | undefined> {
+    const baseURL = import.meta.env.VITE_BASE_URL;
+    try {
+        const response = await axios.post(`${baseURL}/users/login`, {
+            email,
+            password,
+        });
+
+        toast.success(response.data.message);
+        return response.data.data;
+    } catch (error) {
+        if (error instanceof AxiosError) {
+            toast.error(error.response?.data.error);
+        } else if (error instanceof Error) toast.error(error.message);
+    }
+}
