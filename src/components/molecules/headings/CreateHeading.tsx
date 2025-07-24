571a87e9f2139f0b455ae116f7fc5c004b70b3de
Andrea Gomez Alvarez <andreagoal96@gmail.com>
2025-07-24 17:38:02 +0200

    created component CreateHeading.tsx

---
+++ src/components/molecules/headings/CreateHeading.tsx
@@ -0,0 +1,36 @@
+import type { ReactNode } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+import Title from '../../typography/Title';
+import Button from '../../atoms/Button';
+import { PiTrashLight } from 'react-icons/pi';
+
+interface Props {
+    leftIcon?: ReactNode;
+    className?: string;
+    children: ReactNode;
+}
+
+export default function CreateHeading(props: Props) {
+    const { leftIcon, className, children } = props;
+    const classes = twMerge(
+        'text-neutral-primary grid grid-cols-[auto_1fr_auto] items-center gap-2',
+        className
+    );
+
+    return (
+        <>
+            <header className={classes}>
+                {leftIcon && (
+                    <span className="text-neutral-primary">{leftIcon}</span>
+                )}
+                <Title as="h2" variant="title-medium-light">
+                    {children}
+                </Title>
+                <Button buttonVariant="secondary" className="p-2 rounded-full">
+                    <PiTrashLight size={24} />
+                </Button>
+            </header>
+        </>
+    );
+}
