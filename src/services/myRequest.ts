8b9e57723888250a00f08c993c679b3b472ca46d
Andrea Gomez Alvarez <andreagoal96@gmail.com>
2025-07-27 18:08:40 +0200

    created myRequest.ts

---
+++ src/services/myRequest.ts
@@ -0,0 +1,31 @@
+import { toast } from 'react-toastify';
+
+export default async function myRequest(
+    url: string,
+    method: 'POST' | 'GET' | 'PUT' | 'PATCH' | 'DELETE',
+    data?: T
+) {
+    const baseURL = import.meta.env.VITE_SERVER_BASE_URL;
+
+    try {
+        const response = await fetch(`${baseURL}/${url}`, {
+            method: method,
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: data? JSON.stringify(data):undefined,
+        });
+
+        const backendData = await response.json();
+
+        if (!response.ok) {
+            toast.error(backendData.error);
+            return;
+        }
+
+        toast.success(backendData.message);
+        return backendData.data;
+    } catch (error) {
+        if (error instanceof Error) toast.error(error.message);
+    }
+}
