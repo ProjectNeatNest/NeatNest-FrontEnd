f9ca0f80de47a8943e7991ace564203da2ec6ed2
Andrea Gomez Alvarez <andreagoal96@gmail.com>
2025-07-27 18:12:45 +0200

    created logOut button in MyProfilePage

--- src/pages/MyProfilePage.tsx
+++ src/pages/MyProfilePage.tsx
@@ -1,7 +1,20 @@
+import { PiSignOutLight } from 'react-icons/pi';
+
+import Button from '../components/atoms/Button';
+import useUserContext from '../hooks/useUserContext';
+
 export default function MyProfilePage() {
+    const { logoutUser } = useUserContext();
+
     return (
-    <>
-        
-    </>
-    )
+        <div className=''>
+            <Button
+                buttonVariant="secondary"
+                icon={<PiSignOutLight size={24} />}
+                onClick={logoutUser}
+            >
+                Cerrar sesión
+            </Button>
+        </div>
+    );
 }
