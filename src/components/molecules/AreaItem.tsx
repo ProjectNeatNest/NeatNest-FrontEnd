78d0f13a013bf5425d546d369a92039fe5eb9166
Andrea Gomez Alvarez <andreagoal96@gmail.com>
2025-07-27 18:17:50 +0200

    created AreaList

--- src/components/molecules/AreaItem.tsx
+++ src/components/molecules/AreaItem.tsx
@@ -1,20 +1,25 @@
+import type { Area } from '../../config/types';
 import Title from '../typography/Title';
 import NumberOfTasks from './NumberOfTasks';
 
 interface Props {
-    areaName: string;
+    area: Area;
     completedTasks: number;
     totalTasks: number;
 }
 
 export default function AreaItem(props: Props) {
-    const { areaName, completedTasks, totalTasks } = props;
+    const { area, completedTasks, totalTasks } = props;
 
     return (
         <>
             <div className="flex flex-col items-center justify-center px-4 py-2 bg-neutral-tertiary rounded-xl min-w-[108px] max-w-[130px] aspect-square shadow-mds">
-                <Title as="h3" variant="title-xsmall-regular">
-                    {areaName}
+                <Title
+                    as="h3"
+                    variant="title-xsmall-regular"
+                    className="text-neutral-primary"
+                >
+                    {area.name}
                 </Title>
                 <div className="flex items-end gap-1 justify-baseline text-neutral-secondary">
                     <NumberOfTasks
