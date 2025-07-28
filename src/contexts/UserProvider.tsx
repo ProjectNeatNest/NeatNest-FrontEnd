f2bc4b7ad952e59cdfd65dfdde2f9bf887347ba3
Andrea <andreagoal96@gmail.com>
2025-07-28 09:55:17 +0200

    added navigate to logoutUser function from UserProvider

--- src/contexts/UserProvider.tsx
+++ src/contexts/UserProvider.tsx
@@ -1,6 +1,7 @@
 import { useState, type ReactNode } from 'react';
-
+import { useNavigate } from 'react-router';
 import { UserContext } from './UserContext';
+
 import type { User } from '../config/types';
 
 interface Props {
@@ -9,6 +10,7 @@ interface Props {
 
 export default function UserProvider(props: Props) {
     const { children } = props;
+    const navigate = useNavigate();
 
     const [user, setUser] = useState<User | null>(() => {
         const userLS = localStorage.getItem('neat-nest-user');
@@ -25,6 +27,7 @@ export default function UserProvider(props: Props) {
     const logoutUser = () => {
         setUser(null);
         localStorage.removeItem('neat-nest-user');
+        navigate('/');
     };
 
     const context = { user, loginUser, logoutUser };
