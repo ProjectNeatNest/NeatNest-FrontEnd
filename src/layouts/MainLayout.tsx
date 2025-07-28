55099ffc2eca5da28c86333904a17ba9b76c75b2
Andrea <andreagoal96@gmail.com>
2025-07-28 09:56:08 +0200

    wrapped main from MainLayout with UserProvider and deleted from main.ts

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,16 +3,19 @@ import { Outlet } from 'react-router';
 import Footer from '../components/molecules/Footer';
 import Header from '../components/molecules/Header';
 import { ToastContainer } from 'react-toastify';
+import UserProvider from '../contexts/UserProvider';
 
 export default function MainLayout() {
     return (
-        <div className="flex flex-col min-h-screen bg-neutral-primary">
-            <Header />
-            <main className="grid p-6 md:px-32 place-items-center">
-                <Outlet />
-            </main>
-            <Footer />
-            <ToastContainer />
-        </div>
+        <UserProvider>
+            <div className="flex flex-col min-h-screen bg-neutral-primary">
+                <Header />
+                <main className="grid p-6 grow md:px-32 justify-items-center">
+                    <Outlet />
+                </main>
+                <Footer />
+                <ToastContainer position="bottom-right" />
+            </div>
+        </UserProvider>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 
-import './index.css';
-
-import UserProvider from './contexts/UserProvider.tsx';
 import { router } from './router.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <UserProvider>
             <RouterProvider router={router} />
-        </UserProvider>
     </StrictMode>
 );
