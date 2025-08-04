40f3a69cf71e084247d5cf76ecb6e07a0287013f
Nastya Rosa <nastyaagb@gmail.com>
2025-08-04 10:53:15 +0200

    new spinner

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,7 +1,7 @@
 export default function Spinner() {
     return (
     <>
-        <img src="/Loading.gif" alt="Loading Spinner"/>
+        <img src="/Loading.svg" alt="Loading Spinner" className="max-w-20 "/>
     </>
     )
 }
